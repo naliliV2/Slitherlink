b112bb2f80141385d840e841c8df886639895acc
nalili <51997434+naliliV2@users.noreply.github.com>
2021-02-18 04:18:37 +0100

    1.4.1
    Finish the fonction "around"

--- check_rules.py
+++ check_rules.py
@@ -1,4 +1,5 @@
 def rules(size, grid):
+    grod = around(size, grid)
     grid = acces_void(size, grid) 
     grid = max_take(size, grid) 
     return grid
@@ -28,30 +29,64 @@ def around(size, grid):
                     else: 
                         temp[len(temp):] = [2, grid[line-1][comlumn], grid[line-1][comlumn+1]]
                         temp[len(temp):] = [grid[line][comlumn+1]]
-                        temp[len(temp):] = [grid[line+1][comlumn+1], grid[line+1][comlumn], 22]
+                        temp[len(temp):] = [grid[line+1][comlumn+1], grid[line+1][comlumn], 2]
                         temp[len(temp):] = [2]
-                if comlumn == size-1:
+                elif comlumn == size-1:
                     if line == 0:
-                        temp[len(temp):] = [grid[line-1][comlumn-1], grid[line-1][comlumn], grid[line-1][comlumn+1]]
-                        temp[len(temp):] = [grid[line][comlumn+1]]
-                        temp[len(temp):] = [grid[line+1][comlumn+1], grid[line+1][comlumn], grid[line+1][comlumn-1]]
+                        temp[len(temp):] = [2, 2, 2]
+                        temp[len(temp):] = [2]
+                        temp[len(temp):] = [2, grid[line+1][comlumn], grid[line+1][comlumn-1]]
                         temp[len(temp):] = [grid[line][comlumn-1]]
                     elif line == size-1:
-                    else: 
-                if line == 0:
-                    if comlumn == 0:
-                    elif comlumn == size-1:
-                    else: 
-                if line == size-1:
-                    if comlumn
+                        temp[len(temp):] = [grid[line-1][comlumn-1], grid[line-1][comlumn], 2]
+                        temp[len(temp):] = [2]
+                        temp[len(temp):] = [2, 2, 2]
+                        temp[len(temp):] = [grid[line][comlumn-1]]
+                    else:
+                        temp[len(temp):] = [grid[line-1][comlumn-1], grid[line-1][comlumn], 2]
+                        temp[len(temp):] = [2]
+                        temp[len(temp):] = [2, grid[line+1][comlumn], grid[line+1][comlumn-1]]
+                        temp[len(temp):] = [grid[line][comlumn-1]] 
+                elif line == 0:
+                    temp[len(temp):] = [2, 2, 2]
+                    temp[len(temp):] = [grid[line][comlumn+1]]
+                    temp[len(temp):] = [grid[line+1][comlumn+1], grid[line+1][comlumn], grid[line+1][comlumn-1]]
+                    temp[len(temp):] = [grid[line][comlumn-1]] 
+                elif line == size-1:
+                    temp[len(temp):] = [grid[line-1][comlumn-1], grid[line-1][comlumn], grid[line-1][comlumn+1]]
+                    temp[len(temp):] = [grid[line][comlumn+1]]
+                    temp[len(temp):] = [2, 2, 2]
+                    temp[len(temp):] = [grid[line][comlumn-1]] 
+            
+            temp2 = temp.count(2)
+            nb = 0
+            enclenched = 0
+            for i in range(8):
+                if enclenched == 1:
+                    if temp2 == 2:
+                        nb+=1
+                        if nb == temp2:
+                            grid[line][comlumn] = 1
+                            break #C'est bon
+                    else:  
+                        grid[line][comlumn] = 0
+                        break #C'est pas bon
+                else:
+                    if temp2 == 2:
+                        enclenched = 1
+    return grid
 
 def acces_void(size, grid): #Verified 
     for line in range(1, size-1, 1):
         for column in range(1, size-1, 1):
             if grid[line][column] == 2:
                 break
+            
             elif grid[line-1][column] == 2 or grid[line+1][column] == 2 or grid[line][column-1] == 2 or grid[line][column+1] == 2:  
                 grid[line][column] = 1
+            
+            elif grid[line-1][column] == 0 or 1 and grid[line+1][column] == 0 or 1 and grid[line][column-1] == 0 or 1 and grid[line][column+1] == 0 or 1:
+                grid[line][column] = 0
     return grid
 
 def max_take(size, grid): #Verified ##Manque vertical x
